ed890ea8874980e77a076eb48c46bc6a9053692b
David Verbenyi <dverbenyi@gmail.com>
2018-08-24 15:50:15 +0100

    Initial Commit

---
+++ Project-16-JavaScript-Injection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project-16-JavaScript-Injection
+//
+//  Created by verebes on 23/08/2018.
+//  Copyright © 2018 A&D Progress. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
